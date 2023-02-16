8bccee3e69e4e72ce4d450420122c2d68647bc7c
SW-Muriu <sw.muriu@gmail.com>
2023-02-16 18:23:18 +0300

    C program that outputs a given statement

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#inlude <stdio.h>
+
+   /*
+    *first c program
+    * out puts a statement
+   */
+int main(void)
+{
+  puts("\" Programming is like building a multilingual puzzle");
+  return (0);
+}
