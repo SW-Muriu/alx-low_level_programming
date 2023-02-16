18b6d3107f96eaefea57dbc8176903fd48e3ce1e
SW-Muriu <sw.muriu@gmail.com>
2023-02-17 00:11:26 +0300

    Correction for Betty documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-   /**
-    * Main - Entry point
-    * Description: First C programme at ALx, Outputs a given Statement
-    * Return: Always 0 (Success)
-    */
+ /**
+  * Main - Entry point
+  * Description: First C programme at ALx, Outputs a given Statement
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
