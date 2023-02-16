c663c4a8773646bbe04dfeb6a660dda8bc44726a
SW-Muriu <sw.muriu@gmail.com>
2023-02-16 18:55:51 +0300

    C program that outputs a given statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
    /**
-    *main - Entry point
     *Description: First C programme at ALx, Outputs a given Statement
     *Return: 0
     */
-    
-int main(void)
+ int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
