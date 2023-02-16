1c7c2ff4f89712c1c00a0af958ea0ad205bcf8fc
SW-Muriu <sw.muriu@gmail.com>
2023-02-16 23:57:42 +0300

    Correction 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
     * Description: First C programme at ALx, Outputs a given Statement
     * Return: Always 0 (Success)
     */
- int main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
