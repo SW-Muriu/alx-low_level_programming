d2b71f11f41fbdf732c4b5d054ff0076e82989a1
SW-Muriu <sw.muriu@gmail.com>
2023-02-16 18:31:15 +0300

    C program that outputs a given statement

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+   /**
+    * First C programme at ALX
+    * Outputs a given Statement
+    */
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle");
+  return 0;
+}
