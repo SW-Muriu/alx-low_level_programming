ced61f0a2d07c968ee963aac75230aa6caefee24
SW-Muriu <sw.muriu@gmail.com>
2023-02-22 20:37:26 +0300

    Computes whether a value is an alphabet or an integer

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - edit the absolute value of an integer
+ * @c: int type 
+ *
+ * Return: absolute value of @c
+ */
+int _abs(intcn)
+{
+	if (c < 0)
+	{
+		return (c * -1);
+	}
+	else
+	{
+		return (c);
+	}
+}
