a6000ffe730a319a89438c2d7cba3c296ca305ae
SW-Muriu <sw.muriu@gmail.com>
2023-02-22 19:46:36 +0300

    Correction 1.0: revised betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * _islower - returns 1 if c is lowercase
+ * _islower - returns 1 if c = lowercase, 
+ * 0 otherwise
  * @c : character for lowercased
  *
- * returns 0: Otherwise
+ * returns: 0 if lowercased, 1 if uppercased
  */
 int _islower(int c)
 {
-	if (c > 96 && c <123)
+	if (c > 96 && c < 123)
 		return (1);
 	else
 		return (0);
---
+++ 0x02-functions_nested_loops/3-islower.c~
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - returns 1 if c is lowercase
+ * @c : character for lowercased
+ *
+ * returns 0: Otherwise
+ */
+int _islower(int c)
+{
+	if (c > 96 && c <123)
+		return (1);
+	else
+		return (0);
+}
