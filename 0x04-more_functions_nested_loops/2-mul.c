56e1754afad8b05f53547781bdd2d5f7299c4168
SW-Muriu <sw.muriu@gmail.com>
2023-02-24 01:24:19 +0300

    Initial Commit: Finds the product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ * @m: integer parameter
+ * @n: second integer parametre
+ *
+ * Return: the product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
