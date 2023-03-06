4cb4b69542622db9fea5de8d8ded26bec4bdff9f
SW-Muriu <87295598+SW-Muriu@users.noreply.github.com>
2023-03-06 22:51:40 +0300

    Update 7-print_chessboard.c
    Mobile updates

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,28 +1,51 @@
-
-#include "main.h" 
+#include "main.h" 
   
  /** 
-  * print_chessboard - print the chessboard, 2d array 
-  * @a: 2d array of chars 
-  */ 
+  * print_chessboard - print the chessboard, 2d array 
+  * @a: 2d array of chars 
+  */ 
   
- void print_chessboard(char (*a)[8]) 
+ void print_chessboard(char (*a)[8]) 
  { 
-         int y, x; 
+         int y, x; 
   
          y = 0;
          while (y < 8) 
-         { 
-                 x = 0; 
-                 while (x < 8) 
-                 { 
-                         _putchar(a[y][x]); 
-                         x++; 
-                 } 
-                 _putchar('\n'); 
-                 y++; 
-         } 
+         { 
+                 x = 0; 
+                 while (x < 8) 
+                 { 
+                         _putchar(a[y][x]); 
+                         x++; 
+                 } 
+                 _putchar('\n'); 
+                 y++; 
+         } 
  }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
