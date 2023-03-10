85a17690e774408283310dc563e48004a88c8a61
SW-Muriu <sw.muriu@gmail.com>
2023-03-11 01:44:25 +0300

    Update 1.2: Revised all code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,137 +1,25 @@
-#include <stdio.h>
 #include "main.h"
-int _strlen(char *s);
-int checkint(char c);
-long int _atoi(char *n, int len);
-long int _pow(int n, int i);
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - the father of the program
- * program multiples 2 numbers
- * print error if two numbers are not apparent 
- * @argc: contains command line arugument count
- * @argv: contain char string pointers of command line args
- *
- * Return: always 0
+ * main - multiply 2 numbers passed to main, or Error
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 if error, 0 if function runs correctly
  */
 
 int main(int argc, char *argv[])
 {
-	int a = _atoi(argv[2], _strlen(argv[2]));
-	int b = _atoi(argv[1], _strlen(argv[1]));
-	int result = a * b;
+	(void) argc;
 
-	if (argc == 1)
+	if (argv[1] && argv[2])
 	{
-		puts("Error");
-		return (1);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
-	printf("%ld\n", result);
-	return (0);
-}
+	else
+		printf("Error\n");
 
-/**
- * _strlen - calculates length of string
- * @s: a pointer to the string
- *
- * Return: length of string.
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-	int i = 0;
-
-	while (1)
-	{
-		if (*(s + i) != '\0')
-		{
-			len++;
-			i++;
-		}
-		else
-		{
-			return (len);
-		}
-	}
 	return (1);
-}
-
-/**
- * _atoi - convertss string into integer.
- * @n: a pointer to the string.
- * @len: the length of the string.
- *
- * Return: an integer value else -1
- */
-
-long int _atoi(char *n, int len)
-{
-	int i;
-	int num = 0;
-
-	for (i = 0; i < len; i++)
-	{
-		if (checkint(*(n + len - i - 1)) == '-')
-		{
-			return (-num);
-		}
-		else if (checkint(*(n + len - i - 1)) == '+')
-		{
-			return (+num);
-		}
-		num += checkint(*(n + len - i - 1)) * _pow(10, i);
-	}
-	return (num);
-}
-
-/**
- * checkint - check if a character value is equal to an int.
- * @c: the character value.
- *
- * Return: an int value or -1
- */
-
-int checkint(char c)
-{
-	int i;
-	int j = 0;
-
-	for (i = 48; i < 58; i++)
-	{
-		if (c == i)
-		{
-			return (j);
-		}
-		j++;
-	}
-	if (c == '-')
-	{
-		return ('-');
-	}
-	return ('+');
-}
-
-/**
- * _pow - does power calculations on integer values.
- * @n: the base.
- * @i: the indice.
- *
- * Return: n power i
- */
-
-long int _pow(int n, int i)
-{
-	int j;
-	int pow = 1;
-
-	if (i == 0)
-	{
-		return (1);
-	}
-	for (j = 0; j < i; j++)
-	{
-		pow *= n;
-	}
-	return (pow);
 }
