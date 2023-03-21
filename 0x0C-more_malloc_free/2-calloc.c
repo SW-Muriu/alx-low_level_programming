e2834f6867aa6dd6a6d048a725b88288aa6c8f91
SW-Muriu <sw.muriu@gmail.com>
2023-03-21 12:38:40 +0300

    The one in front of the gun lives forever

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,27 +1,46 @@
-#include "main.h"
 #include <stdlib.h>
-/**
- *_calloc - allocate array of size * nmemb.
- * @nmemb: number of elements.
- * @size: size of element.
- * Return: pointer to array.
- **/
+#include "main.h"
 
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *tab;
-	unsigned int i;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	tab = malloc(size * nmemb);
+	ptr = malloc(size * nmemb);
 
-	if (tab == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < (size * nmemb); i++)
-		tab[i] = 0;
+	_memset(ptr, 0, nmemb * size);
 
-	return (tab);
+	return (ptr);
 }
